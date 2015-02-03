cdaee6d3c4eb4987f4c669a389e44885d613cb29
Alejo VM10 <alejovelez10@gmail.com>
2015-02-03 17:06:23 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GagCf::Application.config.secret_token = '0a56f30f39e567d37da5ff0da8216723ec95b37ede44e70191372cdb0f596d2bec82841ca06ab7fe609ca61243a5610d568b7139a3c584884f8926350b83c108'
